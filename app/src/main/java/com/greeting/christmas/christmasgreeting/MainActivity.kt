5158503c47d151ebd9614a8942affea05bbf3f19
yash30401 <yashveersingh30401@gmail.com>
2022-12-23 13:29:57 +0530

    1. Card Acitivty Created

--- app/src/main/java/com/greeting/christmas/christmasgreeting/MainActivity.kt
+++ app/src/main/java/com/greeting/christmas/christmasgreeting/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.greeting.christmas.christmasgreeting
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -23,10 +24,56 @@ class MainActivity : AppCompatActivity() {
                 var name=binding.name.text.toString()
 
                 val intent=Intent(this,cardActivity::class.java)
-                intent.putExtra("santa",R.drawable.santa)
+                intent.putExtra("card","santa")
                 intent.putExtra("name",name)
                 startActivity(intent)
             }
+
+        }
+
+        binding.poster.setOnClickListener {
+
+            if(binding.name.text.isNullOrEmpty()){
+                Toast.makeText(applicationContext, "Please Type Name ", Toast.LENGTH_SHORT).show()
+            }else{
+                var name=binding.name.text.toString()
+
+                val intent=Intent(this,cardActivity::class.java)
+                intent.putExtra("card","poster")
+                intent.putExtra("name",name)
+                startActivity(intent)
+            }
+
+        }
+
+        binding.tree.setOnClickListener {
+
+            if(binding.name.text.isNullOrEmpty()){
+                Toast.makeText(applicationContext, "Please Type Name ", Toast.LENGTH_SHORT).show()
+            }else{
+                var name=binding.name.text.toString()
+
+                val intent=Intent(this,cardActivity::class.java)
+                intent.putExtra("card","tree")
+                intent.putExtra("name",name)
+                startActivity(intent)
+            }
+
+        }
+
+        binding.deer.setOnClickListener {
+
+            if(binding.name.text.isNullOrEmpty()){
+                Toast.makeText(applicationContext, "Please Type Name ", Toast.LENGTH_SHORT).show()
+            }else{
+                var name=binding.name.text.toString()
+
+                val intent=Intent(this,cardActivity::class.java)
+                intent.putExtra("card","deer")
+                intent.putExtra("name",name)
+                startActivity(intent)
+            }
+
 
         }
 
--- app/src/main/java/com/greeting/christmas/christmasgreeting/cardActivity.kt
+++ app/src/main/java/com/greeting/christmas/christmasgreeting/cardActivity.kt
@@ -2,10 +2,40 @@ package com.greeting.christmas.christmasgreeting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.greeting.christmas.christmasgreeting.databinding.ActivityCardBinding
 
 class cardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card)
+        binding=ActivityCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var name=intent.getStringExtra("name")
+        var card=intent.getStringExtra("card")
+
+
+        binding.name.text="Merry Christmas\n"+name.toString()
+
+        if(card=="santa"){
+
+           binding.imageView.setBackgroundResource(R.drawable.santa)
+
+        }else if(card=="poster"){
+
+            binding.imageView.setBackgroundResource(R.drawable.poster)
+
+        }else if(card=="tree"){
+
+            binding.imageView.setBackgroundResource(R.drawable.tree)
+
+        }else if(card=="deer"){
+
+            binding.imageView.setBackgroundResource(R.drawable.deer)
+
+        }
+
     }
 }
